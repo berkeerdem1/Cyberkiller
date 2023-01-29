a2704b6177229d57a85bd8a451ba3971662f530c
Berke Erdem <berkeerdem0@gmail.com>
2023-01-29 18:31:01 +0300

    Add files via upload

---
+++ win.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class win : MonoBehaviour
+{
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if(col.transform.CompareTag("player")){
+			SceneManager.LoadScene(4);
+		}
+		
+	}
+
+}

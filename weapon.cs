617662ecbda213923e6c2a7ecb59a13e65214d6a
Berke Erdem <berkeerdem0@gmail.com>
2023-01-29 18:32:00 +0300

    Add files via upload

---
+++ weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+
+	public Transform Firepoint;
+	public GameObject Bullet;
+
+
+	void Update(){
+		if(Input.GetButtonDown("Fire1")){
+			Shoot();
+		}
+	}
+	void Shoot(){
+
+		Instantiate(Bullet,Firepoint.position,Firepoint.rotation);
+		}
+	
+}

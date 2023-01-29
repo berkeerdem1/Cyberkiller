d027ddccbd97a5e12269516cdad825f962490f67
Berke Erdem <berkeerdem0@gmail.com>
2023-01-29 18:29:32 +0300

    Moving Platform Code

---
+++ movingplatf.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class movingplatf : MonoBehaviour
+{
+	public Transform firstPos,secondPos;
+	public float speed;
+	
+	Vector3 nextPos;
+	
+	private void Start(){
+		nextPos=firstPos.position;
+	}
+	private void Update(){
+		if(transform.position==firstPos.position)
+			nextPos=secondPos.position;
+		if(transform.position==secondPos.position)
+			nextPos=firstPos.position;
+		
+		transform.position=Vector3.MoveTowards(transform.position,nextPos,speed*Time.deltaTime);
+		
+	}
+	
+	private void OnDrawGizmos(){
+		Gizmos.DrawLine(firstPos.position,secondPos.position);
+	}
+	private void OnCollisionEnter2D(Collision2D collision){
+		if(collision.transform.tag=="player")
+			collision.collider.transform.SetParent(transform);
+	}
+	private void OnCollisionExit2D(Collision2D collision){
+		if(collision.transform.tag=="player")
+			collision.collider.transform.SetParent(null);
+	}
+}

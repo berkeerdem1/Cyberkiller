e84f3dae7e43be98b9649bdf04fc367777988cdb
Berke Erdem <berkeerdem0@gmail.com>
2023-01-29 18:30:44 +0300

    Respawn Code

---
+++ spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+	[SerializeField]Transform spawnpoint;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+		if(col.transform.CompareTag("player")){
+			col.transform.position=spawnpoint.position;
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
